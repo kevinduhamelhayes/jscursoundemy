f82f19a757cff3c47187357b66da9d0992316743
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-04-09 18:34:51 -0300

    hola

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log('Hello World');
+//tipos de datos
+//string
+//number
+//boolean
+//null
+//undefined
+//symbol
+//object
+//array
+//function
+let miVariable = 'Hola Mundo';
+console.log(miVariable);
+
+//mutabilidad
+miVariable = 10;
+console.log(miVariable);
+
+let miboolean = true;
+let miotrosboolean = false;
+console.log(miboolean);
+console.log(miotrosboolean);
+
+let minumero = 11;
+let minumero2 = 12;
+let minumero3 = 13;
+console.log(minumero, minumero2, minumero3);
+
+let undefined
+console.log(undefined);
+
+let nulli= null
+console.log(nulli);
+
+//objeto vacio
+const miObjeto = {}
+console.log(miObjeto);
+
+//objeto con propiedades
+const miObjeto2 = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 30
+}
+console.log(miObjeto2.nombre);
+
+//array
+const miArray = [1,2,3,4,5,6,7,8,9,10]
+console.log(miArray[0]);
+
+miArray.push(11)
+console.log(miArray);
+
